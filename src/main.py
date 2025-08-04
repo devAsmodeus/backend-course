d3e3c0c88b2917ef5bd1dc0350e98d36661263b3
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-08-04 17:28:08 +0300

    remove ..

--- src/main.py
+++ src/main.py
@@ -8,14 +8,13 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
-from src.config import settings
 
 app = FastAPI(docs_url=None)
 
 app.include_router(router_hotels)
 
 
-@app.get("../docs", include_in_schema=False)
+@app.get("/docs", include_in_schema=False)
 async def custom_swagger_ui_html():
     return get_swagger_ui_html(
         openapi_url=app.openapi_url,
