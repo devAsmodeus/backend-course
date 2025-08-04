a49a6e9d67a436341c1a7cba8148232b13b393c0
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-08-04 17:25:33 +0300

    add alembic files

---
+++ src/migrations/versions/23ca1892924a_initial_migration.py
@@ -0,0 +1,33 @@
+"""initial migration
+
+Revision ID: 23ca1892924a
+Revises: 
+Create Date: 2025-08-04 17:19:14.310614
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '23ca1892924a'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table('hotels',
+    sa.Column('id', sa.Integer(), nullable=False),
+    sa.Column('title', sa.String(length=100), nullable=False),
+    sa.Column('location', sa.String(), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table('hotels')
