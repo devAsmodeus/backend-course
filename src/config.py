958629aba2253a8ad491d5de261673f9e997a56f
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-08-04 17:27:03 +0300

    add new env variables

--- src/config.py
+++ src/config.py
@@ -3,7 +3,22 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
     DB_NAME: str
+
+    @property
+    def db_url(self) -> str:
+        return (
+            "postgresql+asyncpg://"
+            f"{self.DB_USER}:"
+            f"{self.DB_PASS}@"
+            f"{self.DB_HOST}:"
+            f"{self.DB_PORT}/"
+            f"{self.DB_NAME}"
+        )
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent.parent / ".env"
