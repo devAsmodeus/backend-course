f761d1d1a63a135fc9aacf025b84c51afd53d45c
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-08-18 17:31:55 +0300

    add AuthService

---
+++ src/services/auth.py
@@ -0,0 +1,31 @@
+import jwt
+
+from passlib.context import CryptContext
+from datetime import datetime, timezone, timedelta
+
+from src.config import settings
+
+
+class AuthService:
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+    pwd_context = CryptContext(
+        schemes=["bcrypt"],
+        deprecated="auto"
+    )
+
+    def verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
+
+    def create_access_token(self, data: dict) -> str:
+        to_encode = data.copy()
+        expire = datetime.now(timezone.utc) + timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(
+            payload=to_encode,
+            key=settings.JWT_SECRET_KEY,
+            algorithm=settings.JWT_ALGORITHM
+        )
+        return encoded_jwt
+
+    def hash_password(self, password: str) -> str:
+        return self.pwd_context.hash(password)
