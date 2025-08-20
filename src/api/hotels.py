88554a7c23011a4e7f0e30da769fe2216a6f44f2
devAsmodeus <123vfrc12Pasha2805@!>
2025-08-21 00:04:11 +0300

    remove hotel schema

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Query, Path, Body
 from fastapi.openapi.models import Example
 
-from src.schemas.hotels import Hotel, HotelPATCH, HotelAdd
+from src.schemas.hotels import HotelPATCH, HotelAdd
 from src.api.dependencies import PaginationDep
 from src.database import async_session_maker
 from src.repositories.hotels import HotelsRepository
