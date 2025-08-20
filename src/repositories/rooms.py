1c46dfd13f342d997f9813ccd0c0e154f2451b2a
devAsmodeus <123vfrc12Pasha2805@!>
2025-08-21 00:05:43 +0300

    add rooms schema

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,6 +1,8 @@
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
+from src.schemas.rooms import Rooms
 
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
+    schema = Rooms
