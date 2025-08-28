4321a53c9b2f6fe9bc24e8ea0f21e2c49b2e6689
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-08-28 17:07:10 +0300

    add bookings pydantic schemas

---
+++ src/schemas/bookings.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from datetime import date
+
+
+class BookingsAddRequest(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+class BookingsAdd(BaseModel):
+    room_id: int
+    user_id: int
+    date_from: date
+    date_to: date
+    price: int
