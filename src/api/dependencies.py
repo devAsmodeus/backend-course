d6bb2b332a023a47491de7ca1b7304ddf9514fc1
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-08-28 16:22:56 +0300

    create a database dependencies manager for use async db_session

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -1,7 +1,9 @@
 from fastapi import Depends, Query, Request, HTTPException
 from pydantic import BaseModel
-from typing import Annotated
+from typing_extensions import Annotated
 
+from src.database import async_session_maker
+from src.utils.db_manager import DBManager
 from src.services.auth import AuthService
 
 
@@ -30,3 +32,11 @@ def get_current_user_id(
 
 
 UserIdDep = Annotated[int, Depends(get_current_user_id)]
+
+
+async def get_db():
+    async with DBManager(session_factory=async_session_maker) as db:
+        yield db
+
+
+DBDep = Annotated[DBManager, Depends(get_db)]
