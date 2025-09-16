758fdad8556614ce521d45e1523cce74d303a763
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-09-16 15:00:32 +0300

    add get_filtered_by_time method

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,8 +1,25 @@
+from datetime import date
+
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
 from src.schemas.rooms import Room
+from src.repositories.utils import rooms_ids_for_booking
 
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
     schema = Room
+
+    async def get_filtered_by_time(
+            self,
+            hotel_id: int,
+            date_from: date,
+            date_to: date,
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            hotel_id=hotel_id,
+            date_from=date_from,
+            date_to=date_to
+        )
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
