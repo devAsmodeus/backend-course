d0ad914327079f255e57fcf009b8996f2c858f62
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-09-16 15:06:30 +0300

    remove method get_all

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -12,11 +12,27 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
+    async def get_filtered_by_time(
             self,
-
+            date_from: date,
+            date_to: date,
+            title: str | None,
+            location: str | None,
+            limit: int,
+            offset: int,
     ) -> list[Hotel]:
-        query = select(HotelsOrm)
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=date_from,
+            date_to=date_to
+        )
+
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
         if title:
             query = query.where(func.lower(HotelsOrm.title).contains(title.lower()))
         if location:
@@ -31,24 +47,3 @@ class HotelsRepository(BaseRepository):
             Hotel.model_validate(hotel, from_attributes=True)
             for hotel in result.scalars().all()
         ]
-
-    async def get_filtered_by_time(
-            self,
-            date_from: date,
-            date_to: date,
-            title: str | None,
-            location: str | None,
-            limit: int,
-            offset: int,
-    ):
-        rooms_ids_to_get = rooms_ids_for_booking(
-            date_from=date_from,
-            date_to=date_to
-        )
-        hotels_ids_to_get = (
-            select(RoomsOrm.hotel_id)
-            .select_from(RoomsOrm)
-            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-        )
-
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
