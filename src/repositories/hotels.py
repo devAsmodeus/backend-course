0cc2317fcf856f8d15007eca69b4ec535d85ca38
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-09-16 14:59:36 +0300

    replace method get_filtered_by_time to utils module

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,8 +1,11 @@
 from sqlalchemy import select, func
+from datetime import date
 
 from src.schemas.hotels import Hotel
 from src.repositories.base import BaseRepository
 from src.models.hotels import HotelsOrm
+from src.models.rooms import RoomsOrm
+from src.repositories.utils import rooms_ids_for_booking
 
 
 class HotelsRepository(BaseRepository):
@@ -11,10 +14,7 @@ class HotelsRepository(BaseRepository):
 
     async def get_all(
             self,
-            title: str | None,
-            location: str | None,
-            limit: int,
-            offset: int
+
     ) -> list[Hotel]:
         query = select(HotelsOrm)
         if title:
@@ -31,3 +31,24 @@ class HotelsRepository(BaseRepository):
             Hotel.model_validate(hotel, from_attributes=True)
             for hotel in result.scalars().all()
         ]
+
+    async def get_filtered_by_time(
+            self,
+            date_from: date,
+            date_to: date,
+            title: str | None,
+            location: str | None,
+            limit: int,
+            offset: int,
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=date_from,
+            date_to=date_to
+        )
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+
+        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
