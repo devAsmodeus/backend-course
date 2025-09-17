c035edd309fb4dfdee5fc7700a60cb012af02a25
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-09-17 23:25:55 +0300

    add set_room_facilities function

--- src/repositories/facilities.py
+++ src/repositories/facilities.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select, delete, insert
+
 from src.repositories.base import BaseRepository
 from src.models.facilities import FacilitiesOrm, RoomsFacilitiesOrm
 
@@ -12,3 +14,40 @@ class FacilitiesRepository(BaseRepository):
 class RoomsFacilitiesRepository(BaseRepository):
     model = RoomsFacilitiesOrm
     schema = RoomFacility
+
+    async def set_room_facilities(
+            self,
+            room_id: int,
+            facility_ids: list[int],
+    ):
+        get_current_facilities_ids_query = (
+            select(self.model.facility_id)
+            .filter_by(room_id=room_id)
+        )
+        result = await self.db_session.execute(get_current_facilities_ids_query)
+        current_facilities_ids: list[int] = result.scalars().all()
+
+        ids_to_delete: list[int] = list(set(current_facilities_ids) - set(facility_ids))
+        ids_to_insert: list[int] = list(set(facility_ids) - set(current_facilities_ids))
+
+        if ids_to_delete:
+            delete_m2m_facilities_stmt = (
+                delete(self.model)
+                .filter(
+                    self.model.room_id == room_id,
+                    self.model.facility_id.in_(ids_to_delete),
+                )
+            )
+            await self.db_session.execute(delete_m2m_facilities_stmt)
+
+        if ids_to_insert:
+            insert_m2m_facilities_stmt = (
+                insert(self.model)
+                .values([
+                    {"room_id": room_id, "facility_id": facility_id}
+                    for facility_id in ids_to_insert
+                ])
+            )
+            await self.db_session.execute(insert_m2m_facilities_stmt)
+
+
