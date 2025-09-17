324e0daabfca33421176b17e0d2794e2588180a1
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-09-17 15:45:25 +0300

    add redirect main page to docs

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import uvicorn
 import sys
 
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from fastapi.openapi.docs import get_swagger_ui_html
 from pathlib import Path
 
@@ -31,6 +32,11 @@ async def custom_swagger_ui_html():
         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
     )
+
+
+@app.get("/", include_in_schema=False)
+async def redirect_docs():
+    return RedirectResponse(url="/docs")
 
 
 if __name__ == "__main__":
