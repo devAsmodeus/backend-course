d946328fbf621125f3eda26eaf242360660ab50e
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-01 12:03:40 +0300

    add redis cache for get facilities

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -1,5 +1,8 @@
+import json
+
 from fastapi import APIRouter
 
+from src.init import redis_manager
 from src.schemas.facilities import FacilitiesAdd
 from src.api.dependencies import DBDep
 
@@ -14,7 +17,17 @@ router = APIRouter(
 async def get_facilities(
         db: DBDep,
 ):
-    return await db.facilities.get_all()
+    facilities_from_cache = await redis_manager.get("facilities")
+    print(f"{facilities_from_cache=}")
+    if not facilities_from_cache:
+        facilities = await db.facilities.get_all()
+        facilities_schemas: list[dict] = [f.model_dump() for f in facilities]
+        facilities_json = json.dumps(facilities_schemas)
+        await redis_manager.set("facilities", facilities_json, expire=30)
+        return facilities
+    else:
+        facilities_dict: list[dict] = json.loads(facilities_from_cache)
+        return facilities_dict
 
 
 @router.post("")
