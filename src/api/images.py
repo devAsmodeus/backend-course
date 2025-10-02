837e0c43285f419532d5627308059e5955fde658
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-02 15:41:36 +0300

    upload income photos

---
+++ src/api/images.py
@@ -0,0 +1,23 @@
+import shutil
+
+from pathlib import Path
+from fastapi import APIRouter, UploadFile
+
+from src.tasks.tasks import resize_image
+
+
+router = APIRouter(
+    prefix="/images",
+    tags=["Изображение отелей"]
+)
+
+
+@router.post("")
+def upload_file(
+        file: UploadFile,
+):
+    image_path = Path(__file__).parent.parent / "static" / "images" / file.filename
+    with open(image_path, "wb+") as new_file:
+        shutil.copyfileobj(file.file, new_file)
+
+    resize_image.delay(str(image_path))
