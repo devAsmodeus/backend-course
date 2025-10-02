2ccd9f3e4cf737d155960331c6cf75346662d891
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-02 17:13:03 +0300

    get bookings with today checkin

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -1,3 +1,7 @@
+from datetime import date
+
+from sqlalchemy import select
+
 from src.repositories.base import BaseRepository
 from src.models.bookings import BookingsOrm
 from src.repositories.mappers.mappers import BookingDataMapper
@@ -6,3 +10,12 @@ from src.repositories.mappers.mappers import BookingDataMapper
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
     mapper = BookingDataMapper
+
+    async def get_bookings_with_today_checkin(self):
+        query = (
+            select(BookingsOrm)
+            .select_from(BookingsOrm)
+            .filter(BookingsOrm.date_from == date.today())
+        )
+        result = await self.db_session.execute(query)
+        return [self.mapper.map_to_domain_entity(booking) for booking in result.scalars().all()]
