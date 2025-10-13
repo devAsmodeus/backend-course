9161d332c22b0b01153e7e0094bdb823a5a5f04b
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-13 17:08:02 +0300

    add test hotel data

---
+++ tests/integration_tests/hotels/test_db.py
@@ -0,0 +1,14 @@
+from src.database import async_session_maker
+from src.schemas.hotels import HotelAdd
+from src.utils.db_manager import DBManager
+
+
+async def test_add_hotel():
+    hotel_data = HotelAdd(
+        title='Some new hotel Dubai Marina 7 stars WOW!',
+        location='Somewhere near where Dubai Marina'
+    )
+    async with DBManager(session_factory=async_session_maker) as db:
+        new_hotel_data = await db.hotels.add(hotel_data)
+        print(f"{new_hotel_data=}")
+        await db.commit()
