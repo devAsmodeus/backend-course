2ab6bea2a92935246a31916aedddd0dde5cd15f5
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-16 16:36:30 +0300

    add async db and ac generators

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import json
 import pytest
 
+from typing import AsyncGenerator
 from httpx import AsyncClient, ASGITransport
 from pathlib import Path
 
@@ -19,50 +20,55 @@ def check_test_mode():
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def setup_database(check_test_mode):
+async def db(check_test_mode) -> AsyncGenerator[DBManager, None]:
+    async with DBManager(session_factory=async_session_maker_null_pool) as db_connection:
+        yield db_connection
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def setup_database(check_test_mode, db):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
 
-@pytest.fixture(scope="session", autouse=True)
-async def mock_hotels(setup_database):
+@pytest.fixture(scope="session")
+async def mock_hotels(setup_database, db):
     json_path = Path(__file__).parent / "mock_hotels.json"
     with open(json_path, 'r', encoding='utf-8') as json_file:
         json_data = json.load(json_file)
 
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        for hotel_data in json_data:
-            hotel_data = HotelAdd(**hotel_data)
-            await db.hotels.add(hotel_data)
-        else:
-            await db.commit()
+    hotels_ = [HotelAdd.model_validate(hotel) for hotel in json_data]
+    await db.hotels.add_bulk(hotels_)
+    await db.commit()
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def mock_rooms(mock_hotels):
+async def mock_rooms(mock_hotels, db):
     json_path = Path(__file__).parent / "mock_rooms.json"
     with open(json_path, 'r', encoding='utf-8') as json_file:
         json_data = json.load(json_file)
 
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        for room_data in json_data:
-            room_data = RoomAdd(**room_data)
-            await db.rooms.add(room_data)
-        else:
-            await db.commit()
+    rooms_ = [RoomAdd.model_validate(hotel) for hotel in json_data]
+    await db.rooms.add_bulk(rooms_)
+    await db.commit()
 
 
-@pytest.fixture(scope="session", autouse=True)
-async def register_user(mock_rooms):
+@pytest.fixture(scope="session")
+async def ac() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(
             transport=ASGITransport(app=app),
             base_url='http://test'
     ) as client:
-        await client.post(
-            url='/auth/register',
-            json={
-                "email": "cat@dog.com",
-                "password": "12345678",
-            }
-        )
+        yield client
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(ac, setup_database):
+    await ac.post(
+        url='/auth/register',
+        json={
+            "email": "cat@dog.com",
+            "password": "12345678",
+        }
+    )
