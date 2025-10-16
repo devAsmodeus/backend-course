78178035e7e1f6e5549d1a429b3e9629cce54d8a
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-16 17:13:42 +0300

    add crud test for booking

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -13,49 +13,33 @@ async def test_create_booking(db):
         date_to=date(2025, 12, 31),
         price=10_000,
     )
-    new_booking_data = await db.bookings.add(booking_data)
-    print(f"{new_booking_data=}")
-    await db.commit()
+    new_booking = await db.bookings.add(booking_data)
+    print(f"{new_booking=}")
 
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+    assert booking.user_id == new_booking.user_id
+    # а еще можно вот так разом сравнить все параметры
+    assert booking.model_dump(exclude={"id"}) == booking_data.model_dump()
 
-# async def test_read_booking(db):
-#     room_id, *_ = await db.rooms.get_all()
-#     user_id, *_ = await db.users.get_all()
-#
-#     booking = await db.bookings.get_one_or_none(
-#         room_id=room_id.id,
-#         user_id=user_id.id
-#     )
-#
-#     assert booking
-#     assert booking is not None
+    # обновить бронь
+    updated_date = date(year=2024, month=8, day=25)
+    update_booking_data = BookingsAdd(
+        user_id=user_id.id,
+        room_id=room_id.id,
+        date_from=date(year=2024, month=8, day=10),
+        date_to=updated_date,
+        price=100,
+    )
+    await db.bookings.edit(update_booking_data, id=new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.date_to == updated_date
 
-
-# async def test_update_booking(db):
-#     room_id, *_ = await db.rooms.get_all()
-#     user_id, *_ = await db.users.get_all()
-#     booking_data = BookingsAdd(
-#         room_id=room_id.id,
-#         user_id=user_id.id,
-#         date_from=date(2025, 1, 1),
-#         date_to=date(2025, 12, 31),
-#         price=10_000,
-#     )
-#     new_booking_data = await db.bookings.add(booking_data)
-#     print(f"{new_booking_data=}")
-#     await db.commit()
-#
-#
-# async def test_delete_booking(db):
-#     room_id, *_ = await db.rooms.get_all()
-#     user_id, *_ = await db.users.get_all()
-#     booking_data = BookingsAdd(
-#         room_id=room_id.id,
-#         user_id=user_id.id,
-#         date_from=date(2025, 1, 1),
-#         date_to=date(2025, 12, 31),
-#         price=10_000,
-#     )
-#     new_booking_data = await db.bookings.add(booking_data)
-#     print(f"{new_booking_data=}")
-#     await db.commit()
+    # удалить бронь
+    await db.bookings.delete(id=new_booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking
