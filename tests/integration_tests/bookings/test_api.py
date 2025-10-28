32d0584ba4e7232bcdaf338fb5804aec3cab81db
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-28 16:25:19 +0300

    add delete all bookings fixture and get my bookings test

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,5 +1,7 @@
 import pytest
 
+from tests.conftest import get_db_null_pool
+
 
 @pytest.mark.parametrize(
     argnames="room_id, date_from, date_to, status_code",
@@ -31,3 +33,43 @@ async def test_add_booking(
     if response.status_code == 200:
         assert response_json["message"] == "Complete"
         assert isinstance(response_json, dict)
+
+
+@pytest.fixture(scope="module")
+async def delete_all_bookings():
+    async for db_ in get_db_null_pool():
+        await db_.bookings.delete()
+        await db_.commit()
+
+
+@pytest.mark.parametrize(
+    argnames="room_id, date_from, date_to, booked_rooms",
+    argvalues=[
+        (1, "2025-01-01", "2025-01-10", 1),
+        (1, "2025-01-01", "2025-01-10", 2),
+        (1, "2025-01-01", "2025-01-10", 3),
+    ]
+)
+async def test_add_and_get_my_bookings(
+        room_id,
+        date_from,
+        date_to,
+        booked_rooms,
+        authenticated_ac,
+        delete_all_bookings
+):
+    response = await authenticated_ac.post(
+        url='bookings',
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+    assert response.status_code == 200
+
+    response = await authenticated_ac.get(
+        url='/bookings/me',
+    )
+    assert response.status_code == 200
+    assert len(response.json()) == booked_rooms
