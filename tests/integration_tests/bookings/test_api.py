19c8f44b680fc4768441f2fc707c016fec1fd8d8
KruglikovskiiPA <p.kruglikovskii@gmail.com>
2025-10-28 16:08:51 +0300

    add parametrize tests for add bookings

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,15 +1,33 @@
-async def test_add_booking(db, authenticated_ac):
-    room_id, *_ = await db.rooms.get_all()
+import pytest
+
+
+@pytest.mark.parametrize(
+    argnames="room_id, date_from, date_to, status_code",
+    argvalues=[
+        (1, "2025-01-01", "2025-01-10", 200),
+        (1, "2025-01-01", "2025-01-10", 200),
+        (1, "2025-01-01", "2025-01-10", 200),
+        (1, "2025-01-01", "2025-01-10", 200),
+        (1, "2025-01-01", "2025-01-10", 200),
+        (1, "2025-01-01", "2025-01-10", 500),
+    ]
+)
+async def test_add_booking(
+        room_id, date_from, date_to, status_code,
+        db, authenticated_ac
+):
+    # room_id, *_ = await db.rooms.get_all()
     response = await authenticated_ac.post(
         url='bookings',
         json={
-            "room_id": room_id.id,
-            "date_from": "2025-01-01",
-            "date_to": "2025-01-10",
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
         }
     )
     response_json = response.json()
 
-    assert response.status_code == 200
-    assert response_json["message"] == "Complete"
-    assert isinstance(response_json, dict)
+    assert response.status_code == status_code
+    if response.status_code == 200:
+        assert response_json["message"] == "Complete"
+        assert isinstance(response_json, dict)
